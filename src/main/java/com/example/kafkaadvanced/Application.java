f0fb496bead3ba119faa4c24513790819f4da180
Ars199 <6653312@gmail.com>
2022-11-13 12:17:25 +0300

    prepare for advanced data

---
+++ src/main/java/com/example/kafkaadvanced/Application.java
@@ -0,0 +1,22 @@
+package com.example.kafkaadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@EnableKafka
+@SpringBootApplication
+public class Application {
+
+    @KafkaListener(topics = "msg")
+    public void msgListener(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+}
