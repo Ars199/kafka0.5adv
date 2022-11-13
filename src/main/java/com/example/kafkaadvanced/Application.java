6faa1b21baa2db227e4feff4ca7498c13194ea51
Ars199 <6653312@gmail.com>
2022-11-13 14:13:46 +0300

    upd

--- src/main/java/com/example/kafkaadvanced/Application.java
+++ src/main/java/com/example/kafkaadvanced/Application.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
-@EnableKafka
+@EnableKafka                                      //консьюмер
 @SpringBootApplication
 public class Application {
 
-    @KafkaListener(topics = "msg")
+    @KafkaListener(topics = "msg")               //слушатель
     public void msgListener(String msg) {
         System.out.println(msg);
     }
--- src/main/java/com/example/kafkaadvanced/MessageController.java
+++ src/main/java/com/example/kafkaadvanced/MessageController.java
@@ -2,19 +2,24 @@ package com.example.kafkaadvanced;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("msg")
-public class MessageController {
+public class MessageController {                                            //продюсер
 
-    @Autowired
+    @Autowired                                                              //инициализация kafkaTemplate
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @PostMapping
+    @PostMapping                                                         //Контроллер мапится на localhost:8080/msg, в теле запроса передаётся ключ и само сообщений
     public void sendOrder(String msgId, String msg){
-        kafkaTemplate.send("msg", msgId, msg);
+        //kafkaTemplate.send("msg", msgId, msg);                           //Имеется несколько перегруженных вариантов данного метода. Используем в нашем проекте вариант с 3 параметрами — send(String topic, K key, V data). Так как KafkaTemplate типизирован String-ом, то ключ и данные в методе send будут являться строкой. Первым параметром указывается топик, то есть тема, в которую будут отправляться сообщения, и на которую могут подписываться консьюмеры, чтобы их получать.
+        ListenableFuture<SendResult<String,String>> future = kafkaTemplate.send("msg", msgId, msg);
+        future.addCallback(System.out::println, System.err::println);     //Метод addCallback() принимает два параметра – SuccessCallback и FailureCallback. Оба они являются функциональными интерфейсами. Из названия можно понять, что метод первого будет вызван в результате успешной отправки сообщения, второго – в результате ошибки
+        kafkaTemplate.flush();                                            //метод println() класса помещает значение в буфер, а уже при flush() толкает в System.out
     }
 }
